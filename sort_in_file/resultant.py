7c7b1df65a2f1acc7076d6a1406b4c70050e6b87
OwnNightmare <yura.borisov.9523@gmail.com>
2021-09-29 19:58:17 +0300

    Made working class file

--- sort_in_file/resultant.py
+++ sort_in_file/resultant.py
@@ -1,30 +1,5 @@
-lines_in_first_part = 0
-with open('story_one.txt', encoding='utf8') as first_part:
-    for line in first_part:
-        lines_in_first_part += 1
-
-lines_in_second_part = 0
-with open('story_two.txt', encoding='utf8') as second_part:
-    for line in second_part:
-        lines_in_second_part += 1
-
-
-lines_in_third_part = 0
-content_third = ''
-with open('story_three.txt', encoding='utf8') as third_part:
-    for line in third_part:
-        content_third += line
-        lines_in_third_part += 1
-    name_third = third_part.name
-
-
-with open('United.txt', 'w', encoding='utf8') as united:
-    united.write(name_third + '\n')
-    united.write(str(lines_in_third_part) + '\n')
-    united.write(content_third)
-
-
 class File:
+    new_str_sign = '\n'
 
     def __init__(self, name):
         self.content = ''
@@ -33,13 +8,37 @@ class File:
 
     def reading(self, mode='r', buffering=1, encoding='utf8'):
         with open(self.name, mode, buffering, encoding) as file:
-            for line_ in file:
-                self.content += line_
+            for line in file:
+                self.content += f"{line.rstrip()}{File.new_str_sign}"
                 self.length += 1
+        self.content = self.content.rstrip()
 
-first_file = File('story_one.txt')
-second_file = File('story_two.txt')
-second_file.reading()
-print(second_file.content)
-print(second_file.name)
-print(second_file.length)
+    def __gt__(self, other):
+        return self.length > other.length
+
+    def __lt__(self, other):
+        return self.length < other.length
+
+    def __eq__(self, other):
+        return self.length == other.length
+
+
+def assign_files():
+    first_file = File('story_one.txt')
+    second_file = File('file_two.txt')
+    third_file = File('story_three.txt')
+    files = [third_file, first_file, second_file]
+    return files
+
+
+def perform_files(files):
+    if type(files) == list:
+        for file in files:
+            file.reading()
+        for file in sorted(files):
+            print(file.name)
+            print(file.length)
+            print(file.content)
+
+
+perform_files(assign_files())
